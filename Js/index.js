26cfbb4e4a745b829d7e78fe43eefca7fb461e01
Tanvi <tanvikarekar22@gmail.com>
2021-08-25 15:40:13 -0500

    first commit

---
+++ Js/index.js
@@ -0,0 +1,20 @@
+document.getElementById('button').addEventListener('click',
+function() {
+    document.querySelector('.popupBox').style.display = 'flex';
+})
+
+
+function change1(){
+    var image = document.getElementById('image');
+    image.src = 'img/hero.png';
+}
+
+function change2(){
+    var image = document.getElementById('image');
+    image.src = 'img/image.png';
+}
+
+function change3(){
+    var image = document.getElementById('image');
+    image.src = 'img/hero.png';
+}
---
+++ css/1.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+   char a = 'a';
+   int b = a;
+   cout<<b;
+   
+    return 0;
+    
+}
